9eb16485cbe1e1d487c487af392ba75422a881b3
Ernest ASwangya <ernest.awangya@gmail.com>
2022-03-15 13:58:26 -0700

    xduyfighlo;

--- terraform/7-ec2-instance.tf
+++ terraform/7-ec2-instance.tf
@@ -19,8 +19,8 @@ resource "aws_instance" "linux_server" {
   subnet_id                   = aws_subnet.subnet-1.id
   vpc_security_group_ids      = [aws_security_group.sg.id]
   associate_public_ip_address = true
-  key_name                    = "ec2-server-key"
-  user_data                   = file("linux-server.sh")
+  key_name                    = "rserver-key"
+  
   
   tags = {
     Name = "Linux-server"
